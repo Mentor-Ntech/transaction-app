4de75e9947ddf763b97d62f1546c63021217d7a5
Mentor-Ntech <102734739+Mentor-Ntech@users.noreply.github.com>
2024-05-10 12:09:30 +0100

    first commit

---
+++ frontend/src/app/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar } from '@/components'
+
+const page = () => {
+  return (
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'>
+         <Navbar />
+      </div>
+     
+    </div>
+  )
+}
+
+export default page
